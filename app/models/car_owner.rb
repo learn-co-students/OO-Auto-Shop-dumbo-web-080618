69a25a424e547eb452b79b9f686d883e6e5c4e83
github username <github email address>
2018-08-16 21:12:27 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,31 @@
 class CarOwner
-
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def list_cars
+    Car.all.select{|name| name.owner == self}
+  # binding.pry
+  end
+  def mechanics
+    # binding.pry
+    list_cars.map{|list| list.mechanic}
   end
 
+  def self.avg_car #remember ALL means classmethod
+  all_owner = self.all.length #num of car_owners
+  all_car = Car.all.length #num of car
+  avg = (all_car / all_owner).round(0) #gives average
+  end
 
 end
+
+
+
+# Get the average amount of cars owned for all owners
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,25 @@
 class Mechanic
-
+  @@all = []
   attr_reader :name, :specialty
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
 end
+def self.all
+  @@all
+end
+# Get a list of all cars that a mechanic services (so calling mechanic3.car_list) will give me name and specialty of mechanic 1- 3
+def car_list
+  Car.all.select{|ser| ser.mechanic == self}
+end
+# Get a list of all the car owners that go to a specific mechanic
 
 
+def car_owners
+  car_list.map{|mk| mk.owner.name}
+  # binding.pry
+
+end
 end
