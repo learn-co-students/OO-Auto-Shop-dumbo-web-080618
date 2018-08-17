c9c3d809e694ebe2bb4cb17f2938ae239ee72bbc
github username <github email address>
2018-08-17 10:46:53 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,8 +2,32 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def all_cars
+    Car.all.select {|c| c.car_owner == self}
+  end
+
+  def all_mechanics
+    self.all_cars.map {|c| c.mechanic}.uniq
+  end
+
+  def self.average_amount
+    aver_amount = 0.0
+    self.all.each{|e| aver_amount += e.all_cars.length}
+    aver_amount/self.all.length
+  end
+
+  def add_car(make, model, classification, mechanic)
+    Car.new(make, model, classification, self, mechanic)
   end
 
 
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -6,6 +6,24 @@ def initialize(name, specialty)
   @name = name
   @specialty = specialty
 end
+def self.all
+  @@all
+end
 
+def add_car(make, model, classification, car_owner)
+  Car.new(make, model, classification, car_owner, self)
+end
+
+def list_of_cars
+  Car.all.select {|c| c.mechanic == self}
+end
+
+def list_of_car_owner
+  self.list_of_cars.map {|c| c.car_owner}.uniq
+end
+
+def list_name_of_car_owner
+  self.list_of_cars.map {|c| c.car_owner.name}.uniq
+end
 
 end
