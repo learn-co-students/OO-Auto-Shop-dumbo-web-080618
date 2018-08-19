baa54e0d71006232aa14e5660716a0d26d05ddbb
dannykrug <dannykrug@gmail.com>
2018-08-19 09:35:04 -0400

    stuck for no reason

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,31 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def cars_owned
+    Car.all.select {|car| car.car_owner == self}
+  end
+
+  def mechanics
+    self.cars_owned.map {|car| car.mechanic}.uniq
+  end
+
+  def self.avg_cars
+    sum = 0
+    self.all.each {|owner| sum += owner.cars_owned.length}
+    sum/all.length
+  end
+
 
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,26 @@
 require 'bundler/setup'
 Bundler.require
 require_all 'app'
+
+require 'pry'
+
+
+danny = CarOwner.new("danny")
+bruce = CarOwner.new("bruce")
+camerons_dad = CarOwner.new("cameron's dad")
+
+doc_brown = Mechanic.new("Doc Brown", "fictional")
+dominic_toretto = Mechanic.new("Dominic Toretto", "muscle")
+brian_the_cop = Mechanic.new("Brian (He's a Cop)", "import")
+
+skyline = Car.new("Nissan", "Skyline", "import", danny, brian_the_cop)
+gto = Car.new("Pontiac", "GTO", "muscle", danny, dominic_toretto)
+delorean = Car.new("Deloran", "Time Machine", "fictional", danny, doc_brown)
+bullitt = Car.new("Ford", "Mustang", "muscle", danny, dominic_toretto)
+batmobile = Car.new("Batmobile", "Tumblr", "fictional", bruce, doc_brown)
+lambo = Car.new("Lamborghini", "Diablo", "import", bruce, brian_the_cop)
+ferrari = Car.new("Ferrari", "250 GT", "import", camerons_dad, brian_the_cop)
+
+dominic_toretto.cars
+
+binding.pry
